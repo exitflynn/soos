911e108d013e5c371c95b6cd9dea01a7b1ed180b
Akshit Tyagi <tyagiakshit833@gmail.com>
2022-12-07 10:20:13 +0530

    hacky soln

--- app.py
+++ app.py
@@ -1,23 +1,129 @@
 from nextcord.ext import commands
-from nextcord import Intents, Member
+from nextcord import Intents
 from journal4 import *;
 
 intents = Intents.default()
 intents.message_content = True
 
-bot = commands.Bot(command_prefix="soos ", intents=intents)
+bot = commands.Bot(command_prefix="", intents=intents)
+
+async def mainboi(ctx, arguments):
+    if "linux" in arguments:
+        await ctx.send(stallman)
+    elif "fuck" in arguments:
+        await ctx.send(mean_msg)
 
 @bot.command(name="hi")
 async def SendMessage(ctx):
     await ctx.send('Hello There!')
 
-@bot.command(name='you', member: Member)
+@bot.command(name='a')
 async def PrettyMuchMain(ctx, *args):
     arguments = ' '.join(args)
-    if "linux" in arguments:
-        await ctx.send(stallman)
-    elif "fuck" in arguments:
-        await ctx.send(mean_msg)
+    await mainboi(ctx, arguments)
+
+@bot.command(name='b')
+async def PrettyMuchMain(ctx, *args):
+    arguments = ' '.join(args)
+    await mainboi(ctx, arguments)
+@bot.command(name='c')
+async def PrettyMuchMain(ctx, *args):
+    arguments = ' '.join(args)
+    await mainboi(ctx, arguments)
+@bot.command(name='d')
+async def PrettyMuchMain(ctx, *args):
+    arguments = ' '.join(args)
+    await mainboi(ctx, arguments)
+@bot.command(name='e')
+async def PrettyMuchMain(ctx, *args):
+    arguments = ' '.join(args)
+    await mainboi(ctx, arguments)
+@bot.command(name='f')
+async def PrettyMuchMain(ctx, *args):
+    arguments = ' '.join(args)
+    await mainboi(ctx, arguments)
+@bot.command(name='g')
+async def PrettyMuchMain(ctx, *args):
+    arguments = ' '.join(args)
+    await mainboi(ctx, arguments)
+@bot.command(name='h')
+async def PrettyMuchMain(ctx, *args):
+    arguments = ' '.join(args)
+    await mainboi(ctx, arguments)
+@bot.command(name='i')
+async def PrettyMuchMain(ctx, *args):
+    arguments = ' '.join(args)
+    await mainboi(ctx, arguments)
+@bot.command(name='j')
+async def PrettyMuchMain(ctx, *args):
+    arguments = ' '.join(args)
+    await mainboi(ctx, arguments)
+@bot.command(name='k')
+async def PrettyMuchMain(ctx, *args):
+    arguments = ' '.join(args)
+    await mainboi(ctx, arguments)
+@bot.command(name='l')
+async def PrettyMuchMain(ctx, *args):
+    arguments = ' '.join(args)
+    await mainboi(ctx, arguments)
+@bot.command(name='m')
+async def PrettyMuchMain(ctx, *args):
+    arguments = ' '.join(args)
+    await mainboi(ctx, arguments)
+@bot.command(name='n')
+async def PrettyMuchMain(ctx, *args):
+    arguments = ' '.join(args)
+    await mainboi(ctx, arguments)
+@bot.command(name='o')
+async def PrettyMuchMain(ctx, *args):
+    arguments = ' '.join(args)
+    await mainboi(ctx, arguments)
+@bot.command(name='p')
+async def PrettyMuchMain(ctx, *args):
+    arguments = ' '.join(args)
+    await mainboi(ctx, arguments)
+@bot.command(name='q')
+async def PrettyMuchMain(ctx, *args):
+    arguments = ' '.join(args)
+    await mainboi(ctx, arguments)
+@bot.command(name='r')
+async def PrettyMuchMain(ctx, *args):
+    arguments = ' '.join(args)
+    await mainboi(ctx, arguments)
+@bot.command(name='s')
+async def PrettyMuchMain(ctx, *args):
+    arguments = ' '.join(args)
+    await mainboi(ctx, arguments)
+@bot.command(name='t')
+async def PrettyMuchMain(ctx, *args):
+    arguments = ' '.join(args)
+    await mainboi(ctx, arguments)
+@bot.command(name='u')
+async def PrettyMuchMain(ctx, *args):
+    arguments = ' '.join(args)
+    await mainboi(ctx, arguments)
+@bot.command(name='v')
+async def PrettyMuchMain(ctx, *args):
+    arguments = ' '.join(args)
+    await mainboi(ctx, arguments)
+@bot.command(name='w')
+async def PrettyMuchMain(ctx, *args):
+    arguments = ' '.join(args)
+    await mainboi(ctx, arguments)
+@bot.command(name='x')
+async def PrettyMuchMain(ctx, *args):
+    arguments = ' '.join(args)
+    await mainboi(ctx, arguments)
+@bot.command(name='y')
+async def PrettyMuchMain(ctx, *args):
+    arguments = ' '.join(args)
+    await mainboi(ctx, arguments)
+@bot.command(name='z')
+async def PrettyMuchMain(ctx, *args):
+    arguments = ' '.join(args)
+    await mainboi(ctx, arguments)
+
+
 
 @bot.event
 async def on_ready():
